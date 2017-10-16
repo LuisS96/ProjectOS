6e4ee247c69fa1ece9a361e90aceebdeb07765ee
OJMS14 <oscar.munoz@cetys.edu.mx>
2017-10-15 18:43:55 -0700

    ya agregue los queues,y para que las ordenes se dividan en ellos

--- 1_Test.py
+++ 1_Test.py
@@ -4,6 +4,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from queue import *
 
+tacos_asada = Queue()
+tacos_otros = Queue()
+tacos_adobada = Queue()
+
 class Orden():
     def __init__(self, Id, Type, meat, qty, ingr, to_go = False):
         self.Id = Id
@@ -21,7 +25,12 @@ def read_order():
     for d in data:
         print('\nNew Order','\nID: ', d['request_id'], '        ',d['datetime'], '\n')
         for i in d['orden']:
-            Orden(i['part_id'], i['type'], i['meat'], i['quantity'], i['ingredients'])
-
-
+            tacos = Orden(i['part_id'], i['type'], i['meat'], i['quantity'], i['ingredients'])
+            if tacos.meat == "asada":
+                tacos_asada.put(d)
+            if tacos.meat == "tripa" or tacos.meat == "cabeza" or tacos.meat == "suadero" or tacos.meat == "lengua":
+                tacos_otros.put(d)
+            if tacos.meat == "adobada":
+                tacos_adobada.put(d)
 read_order()
+
