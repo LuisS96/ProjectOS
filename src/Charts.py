672910b37fe3fb5369decac67931ad8c6b906a29
OJMS14 <oscar.munoz@cetys.edu.mx>
2017-11-19 16:56:03 -0800

    modifications in charts

--- src/Charts.py
+++ src/Charts.py
@@ -1,61 +1,105 @@
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+import pandas as pd
 
-# Receives a DataFrame of at least two keys and two columns
-# In our case, our pie is determined with our column 'Time of completion'
-# Pie chart and bar chart
-def charts(df,order_list):
+#Receives a DataFrame of at least two keys and two columns
+#In our case, our pie is determined with our column 'Time of completion'
+#Pie chart and bar chart
+def charts(answersList):
+    asada_time = []
+    adobada_time = []
+    others_time = []
+    asada_size = 0
+    adobada_size = 0
+    others_size = 0
+    asada_average_time = []
+    adobada_average_time = []
+    others_average_time = []
+    for answer.order in answersList:
+        for suborder in answer.order.suborders:
+            if suborder.meat == 'Asada':
+                asada_time.append((suborder.endTime - suborder.startTime).total_seconds())
+                asada_size += suborder.qty2
+            elif suborder.meat == 'Adobada':
+                adobada_time.append((suborder.endTime - suborder.startTime).total_seconds())
+                adobada_size += suborder.qty2
+            else:
+                others_time.append((suborder.endTime - suborder.startTime).total_seconds())
+                others_size += suborder.qty2
+
+    raw_data_pie = {"Order": [answer.order.Id for answer.order in answersList],
+                "Time of completion": [(answer.order.endTime-answer.order.startTime).total_seconds() for answer.order in answersList]}
+    df_pie = pd.DataFrame(raw_data_pie, columns=['Order', 'Time of completion'])
+    print(df_pie)
+
+    raw_data_bar = {"Order": ['Asada', 'Adobada', 'Others'], "Queue Size": [asada_size,adobada_size,others_size]}
+    df_bar = pd.DataFrame(raw_data_bar, columns=['Order', 'Queue Size'])
+    print(df_bar)
+
+    # raw_data_plot = {'Meat Type':['Asada','Adobada','Others'],
+    #                  'Time Spent': [asada_time, adobada_time, others_time]}
+
     plt.style.use("dark_background")
-    colors = ["cornflowerblue", "orangered", "gold","r","limegreen","m","b","coral","yellow"]
-    fig, axes = plt.subplots(ncols=3, figsize=(15,10))
+    colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', 'grey', 'gold', '#17becf']
+    # grid = GridSpec(1,3)
+
+    fig, axes = plt.subplots(ncols = 3,figsize=(15,10))
     ax1,ax2,ax3 = axes.ravel()
 
     # Table
+    # plt.subplot(grid[0, 0], aspect=1)
     orders = []
-    for order in order_list:
-        orders.append([order.Id,(order.endTime - order.startTime).total_seconds()])
-    # for time in df['Time of completion']:
-    #     orders.append([time])
+    for answer.order in answersList:
+        orders.append([answer.order.Id,(answer.order.endTime - answer.order.startTime).total_seconds()])
     columns = ["Order",'Time of completion']
-    rows = [index for index in range(len(order_list))]
-    ax3.axis('off')
-    table = ax3.table(cellText=orders,
+    rows = [index for index in range(len(answersList))]
+    ax1.axis('off')
+    table = ax1.table(cellText=orders,
               cellColours=[['black'] * len(orders[0])] * len(orders),
               colLabels=columns,
               colColours=['black'] * len(columns),
               rowLabels=rows,
               rowColours=colors,
-              loc='center')
+              loc='center',)
+    table.set_fontsize(25)
     table.scale(1,1.5)
 
     #Pie chart
-    ax1.pie(
-        df['Time of completion'],
-        labels=df['Order'],
+    # plt.subplot(grid[0, 1], aspect=1)
+    ax2.pie(
+        df_pie['Time of completion'],
+        # labels=df_pie['Order'],
         shadow=False,
         colors=colors,
         startangle=90,
         autopct='%1.1f%%',
         )
-    ax1.axis('equal')
-    ax1.margins(1)
+    ax2.axis('equal')
+    ax2.margins(1)
 
     #Bar chart
-    length = list(range(len(df['Time of completion'])))
-    ax2.bar([p + .375 for p in length],
-            df['Time of completion'],
+    # plt.subplot(grid[0,2], aspect=1)
+    length = list(range(len(df_bar['Queue Size'])))
+    ax3.bar([p + .375 for p in length],
+            df_bar['Queue Size'],
             0.25,
             color=colors,
-            label=df["Order"]
+            label=df_bar["Order"]
             )
-    ax2.set_ylabel('Total')
-    ax2.set_title('Tacos')
-    ax2.set_xticks([p + 1.5 * 0.25 for p in length])
-    ax2.set_xticklabels(df['Order'])
+    plt.tight_layout()
 
-    #Both charts will be printed in one space and not one for each.
-    plt.xlim(min(length)-.25, max(length)+0.25*4)
-    plt.ylim([0, max(df['Time of completion']) + 1])
-    plt.grid()
-    fig.tight_layout()
-    fig.subplots_adjust(hspace=100)
+    fig, axes = plt.subplots(ncols = 2,figsize=(10,10))
+    ax4,ax5 = axes.ravel()
+
+    # Plot chart
+    # plt.subplot(grid[1,0], aspect = .5)
+
+    ax4.plot(asada_time,'bo-',label='Asada')
+    ax4.plot(adobada_time,'go-',label='Adobada')
+    ax4.plot(others_time,'ro-',label='Others')
+
+
+
+    plt.tight_layout()
+
     plt.show()
